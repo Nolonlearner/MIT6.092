47c1f1168d7d6c6cd6869bdf814cb622fdd4c92c
Nolonlearner <135989518+Nolonlearner@users.noreply.github.com>
2024-09-12 09:39:00 +0800

    Save local changes before pulling

---
+++ src/lec01/lec01_main.java
@@ -0,0 +1,17 @@
+package lec01;
+public class lec01_main {
+    public static void main(String[] args) {
+        String str;
+        str = "Hello World!";
+        System.out.println(str);
+
+        boolean b = true;
+        System.out.println(b);
+
+        int i = 123;
+        System.out.println(i);
+
+        double d = 3.14;
+        System.out.println(d);
+    }
+}
